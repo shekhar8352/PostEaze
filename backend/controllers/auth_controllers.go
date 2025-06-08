4bbfdff11fb84af79645e6c13539ffc3608f6a87
sudhanshu8352 <shekharsudhanshu8352@gmail.com>
2025-06-08 23:24:42 +0530

    Minor changes in auth controller

--- backend/controllers/auth_controllers.go
+++ backend/controllers/auth_controllers.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
 	"posteaze-backend/services"
 	"posteaze-backend/utils"
@@ -12,15 +13,14 @@ type AuthController struct {
 	Service *services.AuthService
 }
 
-func NewAuthController() *AuthController {
-	return &AuthController{
-		Service: &services.AuthService{},
-	}
+func NewAuthController(service *services.AuthService) *AuthController {
+	return &AuthController{Service: service}
 }
 
 func (a *AuthController) SignupHandler(c *gin.Context) {
 	var body services.SignupParams
 	if err := c.ShouldBindJSON(&body); err != nil {
+		fmt.Println(err)
 		utils.SendError(c, http.StatusBadRequest, "Invalid signup data")
 		return
 	}
@@ -38,14 +38,17 @@ func (a *AuthController) LoginHandler(c *gin.Context) {
 	var body services.LoginParams
 	if err := c.ShouldBindJSON(&body); err != nil || body.Email == "" || body.Password == "" {
 		utils.SendError(c, http.StatusBadRequest, "Email and password are required")
+		utils.Logger.Info("Error binding JSON: ", err)
 		return
 	}
 
 	user, err := a.Service.Login(c.Request.Context(), body)
 	if err != nil {
 		utils.SendError(c, http.StatusUnauthorized, err.Error())
+		utils.Logger.Info("Error logging in user: ", err)
 		return
 	}
 
 	utils.SendSuccess(c, user)
+	utils.Logger.Info("Logged in user successfully: ", user)
 }
