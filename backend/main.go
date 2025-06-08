c52b6307cc318c9d776fcfc7f3f454f45525907c
sudhanshu8352 <shekharsudhanshu8352@gmail.com>
2025-06-08 17:52:02 +0530

    Config file added for DB and Redis

--- backend/main.go
+++ backend/main.go
@@ -5,10 +5,13 @@ import (
 	"log"
 	"os"
 
-	"github.com/joho/godotenv"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+
+	"posteaze-backend/models"
+	"posteaze-backend/pkg/config"
 )
 
 func main() {
@@ -16,29 +19,56 @@ func main() {
 	if err != nil {
 		log.Fatalf("Error loading .env file: %s", err)
 	}
+
 	port := os.Getenv("PORT")
-	db_user := os.Getenv("POSTGRES_USER")
-	db_pass := os.Getenv("POSTGRES_PASSWORD")
-    db_name := os.Getenv("POSTGRES_DB")
+	dbUser := os.Getenv("POSTGRES_USER")
+	dbPass := os.Getenv("POSTGRES_PASSWORD")
+	dbName := os.Getenv("POSTGRES_DB")
+	host := os.Getenv("POSTGRES_HOST")
+	if host == "" {
+		host = "localhost"
+	}
 
 	if port == "" {
 		log.Println("PORT is not found in the environment variable")
 		port = "8080"
 	}
 
-	dsn := "host=localhost user=" + db_user + " password=" + db_pass + " dbname=" + db_name + " port=5432 sslmode=disable TimeZone=Asia/Kolkata"
-	fmt.Println("Connecting to DB...", dsn)
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=5432 sslmode=disable TimeZone=Asia/Kolkata", host, dbUser, dbPass, dbName)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
-	    log.Fatalf("Failed to connect to DB: %v", err)
+		log.Fatalf("Failed to connect to DB: %v", err)
 	}
-	log.Println("Connected to DB successfully!", db)
 
+	// Run extension for UUID generation
+	if err := db.Exec(`CREATE EXTENSION IF NOT EXISTS "pgcrypto"`).Error; err != nil {
+		log.Fatalf("Failed to enable pgcrypto extension: %v", err)
+	}
+
+	// Run DB migrations
+	err = db.AutoMigrate(
+		&models.User{},
+		&models.Team{},
+		&models.TeamMember{},
+	)
+	if err != nil {
+		log.Fatalf("Failed to run migrations: %v", err)
+	}
+
+	config.InitAppContext(db)
 	router := gin.Default()
+
+	// Register routes
+	// authRoutes := router.Group("/api/auth")
+	// {
+	// 	authRoutes.POST("/signup", controllers.SignUp)
+	// 	authRoutes.POST("/login", controllers.Login)
+	// }
+
 	router.GET("/", func(c *gin.Context) {
-	    c.JSON(200, gin.H{
-	        "message": "PostEaze backend running with Gin!",
-	    })
+		c.JSON(200, gin.H{
+			"message": "PostEaze backend running with Gin!",
+		})
 	})
 
 	router.Run(":" + port)
