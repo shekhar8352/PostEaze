820855c6ed0f36873e0f3b04f1e593daafe9509a
sudhanshu8352 <shekharsudhanshu8352@gmail.com>
2025-06-15 18:10:20 +0530

    Add Mantine in the frontend react project

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,14 @@
 import './App.css'
+import '@mantine/core/styles.css';
+
+import { MantineProvider } from '@mantine/core';
 
 function App() {
 
   return (
-    <>
+    <MantineProvider>
       <h1>PostEaze</h1>
-    </>
+    </MantineProvider>
   )
 }
 
