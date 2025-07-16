5901a97e3d90dd7086289bb367ba3ddc7936ecc6
sudhanshu8352 <shekharsudhanshu8352@gmail.com>
2025-07-16 12:27:44 +0530

    Fix errors in frontend project structure

--- frontend/src/features/auth/authRoutes.tsx
+++ frontend/src/features/auth/authRoutes.tsx
@@ -1,4 +1,4 @@
-import { RouteObject } from 'react-router-dom';
+import type { RouteObject } from 'react-router-dom';
 import SignupPage from './pages/SignupPage';
 
 const authRoutes: RouteObject[] = [
