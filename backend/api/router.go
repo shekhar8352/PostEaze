4b43e0e6b6d93798df0e67f972d261ae4dff7bb7
MridulDhahari <mridul.dhari141010@gmail.com>
2025-07-23 03:17:53 +0530

    1) cleaned up main.go 2) added configs 2) created separate API layer with router in it

---
+++ backend/api/router.go
@@ -0,0 +1,38 @@
+package api
+
+import (
+	"fmt"
+
+	"github.com/cloudwego/hertz/pkg/app/server"
+	"github.com/cloudwego/hertz/pkg/route"
+	"github.com/shekhar8352/PostEaze/constants"
+	"github.com/shekhar8352/PostEaze/utils/flags"
+)
+
+// Init is used to initialise the router.
+func Init() error {
+	// create a server
+	s := server.Default(
+		server.WithHostPorts(fmt.Sprintf(":%d", flags.Port())),
+		server.WithKeepAlive(true),
+	)
+
+	v1 := s.Group(constants.V1Route)
+	{
+		addV1UserAuthRoutes(v1)
+	}
+
+	// this is a blocking call unless application receives shut down signal
+	// or some error occurs
+	return s.Run()
+}
+
+func addV1UserAuthRoutes(v1 *route.RouterGroup) {
+	// auth := rg.Group("/auth")
+	authv1 := v1.Group(constants.AuthRoute)
+
+	authv1.POST(constants.SignUpRoute)
+	authv1.POST(constants.LogInRoute)
+	authv1.POST(constants.RefreshRoute)
+	authv1.POST(constants.LogOutRoute)
+}
--- backend/main.go
+++ backend/main.go
@@ -1,73 +1,98 @@
 package main
 
 import (
-	"fmt"
+	"context"
 	"log"
-	"os"
 
-	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
-
-	"posteaze-backend/models"
-	"posteaze-backend/pkg/config"
-	"posteaze-backend/routes"
+	"github.com/shekhar8352/PostEaze/api"
+	"github.com/shekhar8352/PostEaze/constants"
+	"github.com/shekhar8352/PostEaze/utils/configs"
+	"github.com/shekhar8352/PostEaze/utils/flags"
 )
 
 func main() {
-	err := godotenv.Load(".env")
+	// this main function initializes all the services required to run the application e.g database , router , configs , redis etc
+	ctx := context.Background()
+	initConfigs(ctx)
+	initDatabase(ctx)
+	initRouter(ctx)
+
+	// err := godotenv.Load(".env")
+	// if err != nil {
+	// 	log.Fatalf("Error loading .env file: %s", err)
+	// }
+
+	// port := os.Getenv("PORT")
+	// dbUser := os.Getenv("POSTGRES_USER")
+	// dbPass := os.Getenv("POSTGRES_PASSWORD")
+	// dbName := os.Getenv("POSTGRES_DB")
+	// host := os.Getenv("POSTGRES_HOST")
+	// if host == "" {
+	// 	host = "localhost"
+	// }
+
+	// if port == "" {
+	// 	log.Println("PORT is not found in the environment variable")
+	// 	port = "8080"
+	// }
+
+	// dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=5432 sslmode=disable TimeZone=Asia/Kolkata", host, dbUser, dbPass, dbName)
+	// db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	// if err != nil {
+	// 	log.Fatalf("Failed to connect to DB: %v", err)
+	// }
+
+	// // Run extension for UUID generation
+	// if err := db.Exec(`CREATE EXTENSION IF NOT EXISTS "pgcrypto"`).Error; err != nil {
+	// 	log.Fatalf("Failed to enable pgcrypto extension: %v", err)
+	// }
+
+	// // Run DB migrations
+	// err = db.AutoMigrate(
+	// 	&models.User{},
+	// 	&models.Team{},
+	// 	&models.TeamMember{},
+	// 	&models.RefreshToken{},
+	// )
+	// if err != nil {
+	// 	log.Fatalf("Failed to run migrations: %v", err)
+	// }
+
+	// config.InitAppContext(db)
+	// router := gin.Default()
+
+	// // Register routes
+	// routes.RegisterRoutes(router)
+
+	// router.GET("/", func(c *gin.Context) {
+	// 	c.JSON(200, gin.H{
+	// 		"message": "PostEaze backend running with Gin!",
+	// 	})
+	// })
+
+	// router.Run(":" + port)
+}
+
+func initConfigs(ctx context.Context) {
+	var err error
+	configNames := []string{constants.ApplicationConfig, constants.LoggerConfig, constants.DatabaseConfig}
+	if flags.Mode() == constants.DevMode {
+		err = configs.InitDev(flags.BaseConfigPath(), configNames...)
+	} else if flags.Mode() == constants.ReleaseMode {
+		err = configs.InitRelease(flags.Env(), flags.AWSRegion(), configNames...)
+	}
 	if err != nil {
-		log.Fatalf("Error loading .env file: %s", err)
+		log.Fatal(ctx, "error in initialising configs")
 	}
+}
 
-	port := os.Getenv("PORT")
-	dbUser := os.Getenv("POSTGRES_USER")
-	dbPass := os.Getenv("POSTGRES_PASSWORD")
-	dbName := os.Getenv("POSTGRES_DB")
-	host := os.Getenv("POSTGRES_HOST")
-	if host == "" {
-		host = "localhost"
-	}
+func initDatabase(ctx context.Context) {
 
-	if port == "" {
-		log.Println("PORT is not found in the environment variable")
-		port = "8080"
-	}
+}
 
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=5432 sslmode=disable TimeZone=Asia/Kolkata", host, dbUser, dbPass, dbName)
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+func initRouter(ctx context.Context) {
+	err := api.Init()
 	if err != nil {
-		log.Fatalf("Failed to connect to DB: %v", err)
+		log.Fatal(ctx, "error in initialising router", err)
 	}
-
-	// Run extension for UUID generation
-	if err := db.Exec(`CREATE EXTENSION IF NOT EXISTS "pgcrypto"`).Error; err != nil {
-		log.Fatalf("Failed to enable pgcrypto extension: %v", err)
-	}
-
-	// Run DB migrations
-	err = db.AutoMigrate(
-		&models.User{},
-		&models.Team{},
-		&models.TeamMember{},
-		&models.RefreshToken{},
-	)
-	if err != nil {
-		log.Fatalf("Failed to run migrations: %v", err)
-	}
-
-	config.InitAppContext(db)
-	router := gin.Default()
-
-	// Register routes
-	routes.RegisterRoutes(router)
-
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "PostEaze backend running with Gin!",
-		})
-	})
-
-	router.Run(":" + port)
 }
