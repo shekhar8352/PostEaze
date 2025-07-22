9255a15e76e3eac9967c631a7d1d1077f2ecdd80
sudhanshu8352 <shekharsudhanshu8352@gmail.com>
2025-07-23 01:20:39 +0530

    Add comments and file rename

--- frontend/src/features/auth/components/signUp/SignupForm.test.tsx
+++ frontend/src/features/auth/components/signUp/SignupForm.test.tsx
@@ -1,4 +1,3 @@
-// index.test.tsx
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import SignupForm from './index';
--- frontend/src/test/utils/renderWithStore.tsx
+++ frontend/src/test/utils/renderWithStore.tsx
@@ -1,9 +1,18 @@
-// src/test/utils/renderWithStore.tsx
 import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
 import { render } from '@testing-library/react';
 import authReducer from '@/features/auth/authSlice';
 
+/**
+ * Render a component with a redux store.
+ *
+ * By default, it will set up a store with the `authReducer` and no preloaded state.
+ * You can override this by passing a `preloadedState` option, or a fully configured
+ * `store` option.
+ *
+ * It will return an object with the store and the result of calling `render` with
+ * the component wrapped in a `Provider`.
+ */
 export const renderWithStore = (ui: React.ReactElement, { preloadedState = {}, store = configureStore({
   reducer: {
     auth: authReducer,
